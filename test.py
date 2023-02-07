a4fa2de84abc61b56cedf1ea5881afa2c17420a8
Roshan Mathew <roshanpanavelil@gmail.com>
2023-02-07 21:37:21 +0530

    added test.py

---
+++ test.py
@@ -0,0 +1,55 @@
+#print( "enter the first valu" )
+#print" )
+#sum= first_value + second_value
+#print("adittion is %d", sum)
+#sub= first_value - second_value
+#print("substarction is %d", sub)
+#div= first_value / second_value
+#print("division is %d", div)
+
+
+# def cal(question):
+#     sum=0
+#     expression=input(question)  
+#     operands=expression.split('+')
+#     if(expression.isnumeric() == False):
+#      print("invalid expression")
+#     else:
+#      print("valid expression")
+# cal("Enter Expression: ")
+ #for operand in expression:
+ # print(operands)#['99', '665', '565', '3213', '2121', '25']
+    # for operand in operands:
+    #     sum=sum+int(operand)
+    #     print(f"sum= {sum}")
+
+
+# result=0
+# flag=0  
+# expression=input("enter expression: ")
+# operands=expression.split('+,-')
+# for operand in operands:
+#     if (operand.isnumeric()==False or operand==''):
+#         print("invalid expression")
+#         flag=1
+#         break
+#     if(operand.endswith('+','-') and int(operand) < 0 and (operand==int(operand))):
+#         result= result + int(operand)
+#     if(flag!=1):
+#        print(f"result={result}")
+   
+
+
+
+
+
+
+
+
+
+
+
+# expression_valid=[]
+# expression=input("Enter expression: ") #56+96-37-23+89+65-12+69-75
+# expression_list=expression.split('+')
+# 
